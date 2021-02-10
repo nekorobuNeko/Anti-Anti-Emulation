727d9b7c649e048647f458c32a61c87c1fb694e3
nekorobuNeko <78842084+nekorobuNeko@users.noreply.github.com>
2021-02-10 16:12:58 +0900

    Add files via upload

---
+++ deem/deem.py
@@ -0,0 +1,10 @@
+import mypkg
+mypkg.activewindow.CheckWindow()
+mypkg.computername.CheckComputerName()
+mypkg.core.CheckCore()
+mypkg.disksize.CheckDisksize()
+mypkg.file.CheckFile()
+mypkg.macadress.CheckMacAdress()
+mypkg.memory.CheckMemory()
+mypkg.process.CheckProcess()
+mypkg.registry.CheckRegistry()
