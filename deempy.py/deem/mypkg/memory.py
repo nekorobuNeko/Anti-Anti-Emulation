fd046d580ba431e95bc6b651d6253b0b044b22b7
nekorobuNeko <78842084+nekorobuNeko@users.noreply.github.com>
2021-05-14 00:34:14 +0900

    Delete deempy.py directory

--- deempy.py/deem/mypkg/memory.py
+++
@@ -1,8 +0,0 @@
-import os
-import psutil
-def CheckMemory():
-    mem = psutil.virtual_memory().total/1024/1024/1024
-    if mem<4:
-        print("memory size is smaller than 4Gb")
-    else:
-        print("There was no problem with the memory")
